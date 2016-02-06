f1fbedc207eb2e120e4dbe5a509b4f3ba358eb49
David Silver <dsilver@candidatemetrics.com>
2016-02-05 18:21:14 -0800

    show geotweets on map

--- app/controllers/geotweets_controller.rb
+++ app/controllers/geotweets_controller.rb
@@ -1,11 +1,11 @@
 class GeotweetsController < ApplicationController
   def index
     @location = request.location
+    @corners = [@location.longitude-0.5,@location.latitude-0.5,@location.longitude+0.5,@location.latitude+0.5]
     @geotweets = []
-    locations = [@location.longitude-1,@location.latitude-1,@location.longitude+1,@location.latitude+1]
-    TweetStream::Client.new.locations(locations.join(",")) do |status, client|
-      puts status.text
-      @geotweets << status.text
+    TweetStream::Client.new.locations(@corners.join(",")) do |geotweet, client|
+      puts geotweet.text
+      @geotweets << geotweet
       client.stop if @geotweets.size >= 10
     end
     puts "STOP!"
--- app/helpers/geotweets_helper.rb
+++ app/helpers/geotweets_helper.rb
@@ -1,2 +1,9 @@
 module GeotweetsHelper
+  def markers(geotweets)
+    markers = []
+    geotweets.each do |geotweet|
+      markers << URI.encode(geotweet.place.full_name)
+    end
+    return markers
+  end
 end
