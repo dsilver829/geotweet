8d0a0dad1590eec26245a78b473290525c4fda83
David Silver <dsilver@candidatemetrics.com>
2016-02-07 13:43:50 -0800

    add latitude and longitude to Geotweet model

--- app/models/geotweet.rb
+++ app/models/geotweet.rb
@@ -1,5 +1,23 @@
 class Geotweet
   include Elasticsearch::Persistence::Model
+
+  attribute :status, String
+  validates :status, presence: true
+
+  attribute :latitude, Float
+  validates :latitude, presence: true
+
+  attribute :longitude, Float
+  validates :longitude, presence: true
+
+  mapping do
+    indexes :location, type: 'geo_point'
+  end
+
+  def as_indexed_json
+    as_json(location: {lat: lat, lon: lon})
+  end
+
   # Execute code after saving the model.
   #
   after_save { puts "Successfully saved: #{self}" }
--- spec/models/geotweet_spec.rb
+++ spec/models/geotweet_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "geotweet" do
+  let(:geotweet) { Geotweet.create id: 1, status: "Welcome to Twitter!", latitude: 0, longitude: 0 }
+
   it "saves" do
-    article = Geotweet.create id: 1
-    expect(article).to be_persisted
+    expect(geotweet).to be_persisted
+  end
+
+  it "has latitude and longitude fields" do
+    expect(geotweet.latitude).to be_present
+    expect(geotweet.longitude).to be_present
   end
 end
