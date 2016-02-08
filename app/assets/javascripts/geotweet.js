79ea11293e8c85e013918baf83b9301f764bdf37
David Silver <dsilver@candidatemetrics.com>
2016-02-08 12:53:44 -0800

    center map on user location

--- app/assets/javascripts/geotweet.js
+++ app/assets/javascripts/geotweet.js
@@ -3,20 +3,12 @@ $(document).ready(function() {
     console.log("MAP");
 
     handler = Gmaps.build('Google');
-    handler.buildMap({provider: {}, internal: {id: 'map'}}, function () {
-        markers = handler.addMarkers([
-            {
-                "lat": $('#map').data('latitude'),
-                "lng": $('#map').data('longitude'),
-                "picture": {
-                    "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-                    "width": 32,
-                    "height": 32
-                },
-                "infowindow": "hello!"
-            }
-        ]);
-        handler.bounds.extendWith(markers);
-        handler.fitMapToBounds();
-    });
+    handler.buildMap({
+        provider: {
+            zoom:      15,
+            center:    new google.maps.LatLng($('#map').data('latitude'), $('#map').data('longitude')),
+        },
+        internal: {id: 'map'}},
+        function () {}
+    );
 });
