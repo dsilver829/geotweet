eb5486a6ca46c6297ffd0654f6051c50cfcacc0d
David Silver <dsilver@candidatemetrics.com>
2016-02-08 14:02:28 -0800

    remove HTML5 geolocation

--- app/assets/javascripts/geotweet.js
+++ app/assets/javascripts/geotweet.js
@@ -1,23 +1,19 @@
 $(document).ready(function() {
+
     handler = Gmaps.build('Google');
     handler.buildMap({
         provider: {
             zoom:      15,
-            center:    new google.maps.LatLng(0,0),
+            center:    new google.maps.LatLng($('#map').data('latitude'),$('#map').data('longitude')),
 
             //center:    new google.maps.LatLng($('#map').data('latitude'), $('#map').data('longitude')),
         },
         internal: {id: 'map'}},
         function () {
-            if(navigator.geolocation)
-                navigator.geolocation.getCurrentPosition(displayOnMap);
+
     });
 
     google.maps.event.addListener(handler.getMap(), 'idle', function() {
         console.log(handler.getMap().getBounds().toString());
     });
-
-    function displayOnMap(position) {
-        handler.map.centerOn([position.coords.latitude, position.coords.longitude]);
-    }
 });
