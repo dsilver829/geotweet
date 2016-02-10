d63691e09ad43faaad8fb16f1874dd5b994e6b87
David Silver <dsilver@candidatemetrics.com>
2016-02-10 12:31:46 -0800

    do not recreate the database on deployment

--- app/controllers/geotweets_controller.rb
+++ app/controllers/geotweets_controller.rb
@@ -2,17 +2,6 @@ require 'geocoder/results/freegeoip'
 
 class GeotweetsController < ApplicationController
   def index
-    # @location = request.location
-    # @corners = [@location.longitude-0.5,@location.latitude-0.5,@location.longitude+0.5,@location.latitude+0.5]
-    # TweetStream::Client.new.locations(@corners.join(",")) do |geotweet, client|
-    #   coordinates = geotweet.place.bounding_box.coordinates[0]
-    #   (longitude, latitude) = coordinates.transpose.map{|e| e.inject(:+)}.map{|e| e / coordinates.size}
-    #   puts geotweet.text
-    #   @geotweets << geotweet
-    #   client.stop if @geotweets.size >= 10
-    # end
-    # puts "STOP!"
-
     respond_to do |format|
       format.html { @location  = request.location || Geocoder::Result::Freegeoip.new("latitude" => 37.7898, "longitude" => -122.3942)  }
       format.js   { @geotweets = Geotweet.geosearch(top_left: top_left, bottom_right: bottom_right, limit: limit, query: query) }
--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
 Geotweet.__elasticsearch__.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_HOST']
-Geotweet.__elasticsearch__.create_index! force: true
+Geotweet.__elasticsearch__.create_index!
 Geotweet.__elasticsearch__.refresh_index!
