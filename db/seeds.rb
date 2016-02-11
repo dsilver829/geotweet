5504ad28c5d14db9853b060124f62306ae2c71be
David Silver <dsilver@candidatemetrics.com>
2016-02-11 08:37:33 -0800

    update db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 i = 0
 (-90...90).step(10) do |latitude|
   (-180...180).step(10) do |longitude|
-    Geotweet.create(status: "Geotweet ##{i}", latitude: latitude, longitude: longitude)
+    Geotweet.new(latitude: latitude, longitude: longitude, status: "Test ##{i}", user_name: "Tester", user_profile_image_url: "https://pbs.twimg.com/profile_images/666407537084796928/YBGgi9BO_bigger.png", user_screen_name: "test")
     i += 1
   end
 end
