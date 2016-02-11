ebf07da7d23c23d93ea4d65ed1dd33dc7619a2b6
David Silver <dsilver@candidatemetrics.com>
2016-02-10 20:25:01 -0800

    hide time string for now

--- app/helpers/geotweets_helper.rb
+++ app/helpers/geotweets_helper.rb
@@ -8,31 +8,22 @@ module GeotweetsHelper
   end
 
   def time_diff(geotweet)
-    diff = (Time.now - Time.new(geotweet.created_at)).to_i
-    years_diff(diff) || months_diff(diff) || days_diff(diff) || hours_diff(diff) || minutes_diff(diff) || seconds_diff(diff)
+    diff = Time.now - Time.new(geotweet.created_at)
+    diff = (diff / 1000).to_i
+    date_str(geotweet.created_at, diff) || diff_str(diff) || "now"
   end
 
   private
 
-  def years_diff(diff)
-    years = diff / (365 * 24 * 60 * 60)
-    if years > 0
-      "#{years}y"
+  def date_str(creation, diff)
+    if diff < 60 * 60 * 24
+      return nil
     end
+    creation.strftime("%e %b")
   end
 
-  def months_diff(diff)
-    months = diff / (30 * 24 * 60 * 60)
-    if months > 0
-      "#{months}mo"
-    end
-  end
-
-  def days_diff(diff)
-    days = diff / (24 * 60 * 60)
-    if days > 0
-      "#{days}d"
-    end
+  def diff_str(diff)
+    hours_diff(diff) || minutes_diff(diff)
   end
 
   def hours_diff(diff)
@@ -45,7 +36,7 @@ module GeotweetsHelper
   def minutes_diff(diff)
     minutes = diff / 60
     if minutes > 0
-      "#{minutes}min"
+      "#{minutes}m"
     end
   end
 
