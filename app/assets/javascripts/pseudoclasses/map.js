29752da501deddab8e991c9ac8857df2c54104a1
David Silver <dsilver@candidatemetrics.com>
2016-03-14 09:41:28 -0700

    refactor geosearch JavaScript into pseudoclassical pattern

--- app/assets/javascripts/pseudoclasses/map.js
+++ app/assets/javascripts/pseudoclasses/map.js
@@ -9,30 +9,7 @@ var Map = function() {
     this.markers = {};
 
     // Create the search box and link it to the UI element.
-    var input = $('#pac-input')[0];
-    var searchBox = new google.maps.places.SearchBox(input);
-    this.gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Listen for a geographic search
-    searchBox.addListener('places_changed', function() {
-        var places = searchBox.getPlaces();
-
-        if (places.length == 0) {
-            return;
-        }
-
-        // For each place, get the icon, name and location.
-        var bounds = new google.maps.LatLngBounds();
-        var place = places.shift();
-
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        this.gmap.fitBounds(bounds);
-    }.bind(this));
+    new GeosearchForm(this);
 
     var callback = true;
     google.maps.event.addListener(this.gmap, 'bounds_changed', function() {
